889dc9cf77e7c9aee7da58b6dc044d4b169f2b39
inchang <son-inchang@son-inchang-ui-MacBookPro.local>
2022-07-11 17:42:50 +0900

    Initial commit

---
+++ src/main/java/com/ktspace/miniHomeIoT/MiniHomeIoTApplication.java
@@ -0,0 +1,13 @@
+package com.ktspace.miniHomeIoT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniHomeIoTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniHomeIoTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktspace/miniHomeIoT/MiniHomeIoTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktspace.miniHomeIoT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniHomeIoTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
