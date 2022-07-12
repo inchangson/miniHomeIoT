c7f4389ebfcd87efd1a732ce007e645cc8af7612
inchang <inchangson21@gmail.com>
2022-07-12 09:51:57 +0900

    Feat : Create Company

---
+++ src/main/java/com/ktspace/miniHomeIoT/domain/Company.java
@@ -0,0 +1,24 @@
+package com.ktspace.miniHomeIoT.domain;
+
+public class Company {
+    private String id;
+    private String external;
+
+    //region getter and setter
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getExternal() {
+        return external;
+    }
+
+    public void setExternal(String external) {
+        this.external = external;
+    }
+    //endregion
+}
