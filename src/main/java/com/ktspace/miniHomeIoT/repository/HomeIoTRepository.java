81669d5d701b981966f04c377a8560a593e7f491
inchang <3721cha21.11@gmail.com>
2022-07-17 19:04:34 +0900

    Feat : Create Interface of Repository

--- src/main/java/com/ktspace/miniHomeIoT/repository/HomeIoTRepository.java
+++ src/main/java/com/ktspace/miniHomeIoT/repository/HomeIoTRepository.java
@@ -1,5 +1,30 @@
 package com.ktspace.miniHomeIoT.repository;
 
-public interface HomeIoTRepository {
+import com.ktspace.miniHomeIoT.domain.Device;
+import com.ktspace.miniHomeIoT.domain.Resource;
+import com.ktspace.miniHomeIoT.domain.Service;
+import com.ktspace.miniHomeIoT.domain.User;
 
+import java.util.List;
+import java.util.Optional;
+
+public interface HomeIoTRepository {
+    //region User
+    User saveUser(User user);
+    Optional<User> findById(String id);
+    List<Device> findAllDevices(String userId);
+    List<Service> findAllServices(String userId);
+    //endregion
+
+    //region Device
+    Device saveDevice(Device device);
+    void removeDevice(Device device);
+    Optional<Device> findByUser(String userId);
+    Optional<Device> findByService(Integer serviceSeq);
+    List<Resource> findAllResources(Integer deviceSeq);
+    //endregion
+
+    //region Resource
+    Resource saveResource(Resource resource, Integer deviceSeq);
+    //endregion
 }
