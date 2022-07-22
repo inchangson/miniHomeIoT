3c2b1821dec07604131eb52eb5504ed7dc475b4f
inchang <3721cha21.11@gmail.com>
2022-07-22 15:51:54 +0900

    Feat : add constructor and value getter

--- src/main/java/com/ktspace/miniHomeIoT/domain/Resource.java
+++ src/main/java/com/ktspace/miniHomeIoT/domain/Resource.java
@@ -3,4 +3,11 @@ package com.ktspace.miniHomeIoT.domain;
 public class Resource {
     private ResourceGroup resourceGroup;
     private String value;
+    public Resource(ResourceGroup resourceGroup, String value){
+        this.resourceGroup = resourceGroup;
+        this.value = value;
+    }
+    public String getValue() {
+        return value;
+    }
 }
