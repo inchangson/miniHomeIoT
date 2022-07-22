f24e23faaaf73ea19b118ba37efdeea25076c225
inchang <3721cha21.11@gmail.com>
2022-07-22 15:51:06 +0900

    Feat : add get instance by name

--- src/main/java/com/ktspace/miniHomeIoT/domain/ResourceGroup.java
+++ src/main/java/com/ktspace/miniHomeIoT/domain/ResourceGroup.java
@@ -4,6 +4,7 @@ public enum ResourceGroup {
     CONN_STAT("connection-status", "value", "boolean", "연결"),
     PWR_SWITCH("power-switch", "value", "boolean", "파워"),
     OPEN_BUTTON("open-button", "value", "boolean", "열림"),
+    ERROR("N/A", "N/A", "N/A", "해당 리소스 없음"),
     ;
     private final String name;
     private final String code;
@@ -15,6 +16,15 @@ public enum ResourceGroup {
         this.code = code;
         this.valueType = valueType;
         this.korName = korName;
+    }
+
+    public static ResourceGroup findByName(String name){
+        switch (name){
+            case "connection-status" : return CONN_STAT;
+            case "power-switch"      : return PWR_SWITCH;
+            case "open-button"       : return OPEN_BUTTON;
+            default: /*throw error*/return ERROR;
+        }
     }
 
     public String getKorName() {
