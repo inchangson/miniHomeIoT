ae09d26c428db4f7257b2d2b7291648e1097deec
inchang <3721cha21.11@gmail.com>
2022-07-26 15:33:32 +0900

    Refactor : Extract ResponseDTO class to file

---
+++ src/main/java/com/ktspace/miniHomeIoT/dto/ResponseDTO.java
@@ -0,0 +1,24 @@
+package com.ktspace.miniHomeIoT.dto;
+
+//temp class
+public class ResponseDTO{
+    private String resultCode;
+    private Object data;
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
+
