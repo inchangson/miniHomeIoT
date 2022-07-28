98ee73ec00244f481fc37bdf3dddfccbfc8a42bf
inchang <3721cha21.11@gmail.com>
2022-07-28 13:35:59 +0900

    Revert "Fix : Modify ResourceGroup constructor"
    This reverts commit dbbef4db31426519ffa1c623021d5994635dceb0.

--- src/main/java/com/ktspace/miniHomeIoT/domain/enums/ResourceGroup.java
+++ src/main/java/com/ktspace/miniHomeIoT/domain/enums/ResourceGroup.java
@@ -1,43 +1,21 @@
 package com.ktspace.miniHomeIoT.domain.enums;
 
 public enum ResourceGroup {
-    CONN_STAT("connection-status"),
-    PWR_SWITCH("power-switch"),
-    OPEN_BUTTON("open-button"),
-    ERROR("N/A"),
+    CONN_STAT("connection-status", "value", "boolean", "연결"),
+    PWR_SWITCH("power-switch", "value", "boolean", "파워"),
+    OPEN_BUTTON("open-button", "value", "boolean", "열림"),
+    ERROR("N/A", "N/A", "N/A", "해당 리소스 없음"),
     ;
     private final String name;
     private final String code;
     private final String valueType;
     private final String korName;
 
-    ResourceGroup(String name) {
-        switch (name){
-            case "connection-status" :
-                this.name = name;
-                this.code = "value";
-                this.valueType = "boolean";
-                this.korName = "연결";
-                break;
-            case "power-switch"      :
-                this.name = name;
-                this.code = "value";
-                this.valueType = "boolean";
-                this.korName = "파워";
-                break;
-            case "open-button"       :
-                this.name = name;
-                this.code = "value";
-                this.valueType = "boolean";
-                this.korName = "열림";
-                break;
-            default:
-                this.name = "N/A";
-                this.code = "N/A";
-                this.valueType = "N/A";
-                this.korName = "해당 리소스 없음";
-                break;
-        }
+    ResourceGroup(String name, String code, String valueType, String korName) {
+        this.name = name;
+        this.code = code;
+        this.valueType = valueType;
+        this.korName = korName;
     }
 
     public static ResourceGroup findByName(String name){
