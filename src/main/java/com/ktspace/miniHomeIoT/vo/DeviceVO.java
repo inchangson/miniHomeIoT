43e103b9041ea7f6eb1e2e4fcd81207d119453cc
inchang <3721cha21.11@gmail.com>
2022-07-28 10:39:16 +0900

    Feat : Add Device VO

---
+++ src/main/java/com/ktspace/miniHomeIoT/vo/DeviceVO.java
@@ -0,0 +1,11 @@
+package com.ktspace.miniHomeIoT.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DeviceVO {
+    String userId;
+    Integer dvcSeq;
+}
