86bb88dd011b4816758866c95df3529a125d9c8a
inchang <3721cha21.11@gmail.com>
2022-08-02 11:36:59 +0900

    Docs : Add comments following javadoc standard

--- src/main/java/com/ktspace/miniHomeIoT/controller/HomeIoTController.java
+++ src/main/java/com/ktspace/miniHomeIoT/controller/HomeIoTController.java
@@ -9,9 +9,20 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
+/**
+ * HomeIoT의 컨트롤러입니다.
+ * @version 1.0
+ * @author 손인창
+ */
 @RestController
 public class HomeIoTController {
+    /**
+     * 사용자 요청을 처리하는 서비스 객체입니다. {@link HomeIoTService HomeIoTService}
+     */
     private final HomeIoTService homeIotService;
+    /**
+     * 요청에 따른 응답 코드들입니다.
+     */
     private final String CODE_NA = "000";
     private final String CODE_SUCCEED = "200";
 
@@ -21,6 +32,13 @@ public class HomeIoTController {
         this.homeIotService = homeIotService;
     }
 
+    /**
+     * {@link HomeIoTService 서비스}를 통해 온 데이터 값들의
+     * null 체크를 통해 값이 제대로 구해졌는지 확인하고
+     * 그에 따른 결과 코드를 반환합니다.
+     * @param responseDTO
+     * @return 결과 값의 null 여부에 따라 알맞은 code값 반환
+     */
     private String getResultCode(ResponseDTO responseDTO) {
         if (responseDTO.getData() == null) {
             return CODE_NA;
@@ -28,6 +46,11 @@ public class HomeIoTController {
         return CODE_SUCCEED;
     }
 
+    /**
+     * 장치 정보 조회는 {@link HomeIoTService#getUserDevices(String) getUserDevices} 를 참조
+     * @param userId
+     * @return 해당 아이디에 존재하는 모든 장치의 정보(리소스 포함)를 반환
+     */
     @RequestMapping(value = "/devices", method = RequestMethod.GET)
     public ResponseEntity<?> getUserDevices(@RequestHeader("userId") String userId) {
         //장치 목록 조회 : 관련된 리소스 모두 가져옴
@@ -37,6 +60,14 @@ public class HomeIoTController {
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
+    /**
+     * 헤더의 아이디를 통한 사용자 검증과 path parameter 로 넘어오는 장치 seq값을 기준으로
+     * 해당하는 장치 정보를 불러옵니다.
+     * 장치 정보 조회는 {@link HomeIoTService#readDeviceInfo(String, Integer) readDeviceInfo} 를 참조
+     * @param userId
+     * @param dvcSeq
+     * @return 특정 deviceSequence에 해당하는 장치의 정보(리소스 포함)를 반환
+     */
     @RequestMapping(value = "/devices/{deviceSeq}", method = RequestMethod.GET)
     public ResponseEntity<?> readDeviceInfo(@RequestHeader("userId") String userId, @PathVariable(value = "deviceSeq") Integer dvcSeq) {
         //장치 리소스 조회
@@ -46,6 +77,14 @@ public class HomeIoTController {
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
+    /**
+     * 해당하는 장치의 리소스 값(DB)을 변경하고 로그(이력)를 남깁니다.
+     * {@link HomeIoTService#controlResource(String, Integer, String, String) controlResource}를 참조
+     * @param userId
+     * @param dvcSeq
+     * @param map
+     * @return 값 수정 성공 여부를 반환합니다.
+     */
     @RequestMapping(value = "/devices/{deviceSeq}", method = RequestMethod.PUT)
     public ResponseEntity<?> controlResource(@RequestHeader("userId") String userId, @PathVariable(value = "deviceSeq") Integer dvcSeq, @RequestBody HashMap<String, Object> map) {
         //장치 리소스 수정
@@ -57,6 +96,13 @@ public class HomeIoTController {
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
+    /**
+     * 해당하는 장치를 삭제합니다.
+     * {@link HomeIoTService#deleteDevice(String, Integer) deleteDevice}를 참조
+     * @param userId
+     * @param devSeq
+     * @return 장치 삭제 성공 여부를 반환합니다.
+     */
     @RequestMapping(value = "/devices/{deviceSeq}", method = RequestMethod.DELETE)
     public ResponseEntity<?> deleteDevice(@RequestHeader("userId") String userId, @PathVariable(value = "deviceSeq") Integer devSeq) {
         // 장치 삭제
--- src/main/java/com/ktspace/miniHomeIoT/service/HomeIoTService.java
+++ src/main/java/com/ktspace/miniHomeIoT/service/HomeIoTService.java
@@ -1,6 +1,7 @@
 package com.ktspace.miniHomeIoT.service;
 
 import com.ktspace.miniHomeIoT.dto.DeviceStatusDTO;
+import com.ktspace.miniHomeIoT.dto.ResponseDTO;
 import com.ktspace.miniHomeIoT.mapper.DeviceMapper;
 import com.ktspace.miniHomeIoT.mapper.ResourceMapper;
 import com.ktspace.miniHomeIoT.vo.DeviceVO;
@@ -11,28 +12,68 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * HomeIoT의 서비스 입니다.
+ * @version 1.0
+ * @author 손인창
+ */
 @Service
 public class HomeIoTService {
+    /**
+     * 장치 관련 기능 쿼리와 연결되는 매퍼 클래스입니다.
+     */
     DeviceMapper deviceMapper;
+    /**
+     * 리소스 관련 기능 쿼리와 연결되는 매퍼 클래스입니다.
+     */
     ResourceMapper resourceMapper;
+    /**
+     * DB 접근에 따른 결과를 나타내는 코드와 메시지입니다.
+     */
     final int    SUCCEED_CODE   = 200;
     final int    FAIL_CODE      = 500;
     final String SUCCEED_MSG    = "제어 성공";
     final String FAIL_MSG       = "제어 실패";
 
-
+    /**
+     * 생성자주입 방식으로 매퍼 클래스를 불러옵니다.
+     * @param deviceMapper
+     * @param resourceMapper
+     */
     @Autowired
     public HomeIoTService(DeviceMapper deviceMapper, ResourceMapper resourceMapper) {
         this.deviceMapper = deviceMapper;
         this.resourceMapper = resourceMapper;
     }
 
+    /**
+     * 호출은 {@link com.ktspace.miniHomeIoT.controller.HomeIoTController#readDeviceInfo(String, Integer)} 참조
+     * DB조회는 {@link DeviceMapper#findDvcList(DeviceVO) findDvcList} 참조
+     * 장치 sequence 값을 통해 장치 정보를 조회합니다.
+     * 장치 정보: {serviceTargetSeq, external, deviceSeq, modelTypeCode, modelId, deviceName, resource[group, code, value]}
+     * @param userId
+     * @param devSeq
+     * @return 해당하는 장치 정보(단건) 반환
+     */
     public DeviceStatusDTO readDeviceInfo(String userId, Integer devSeq) {
         ArrayList<DeviceStatusDTO> deviceStatusList = deviceMapper.findDvcList(new DeviceVO(userId, devSeq));
-        DeviceStatusDTO result = deviceMapper.findDvcList(new DeviceVO(null, devSeq)).get(0);
+        /**
+         * 매퍼 클래스의 반환 값이 리스트 형태이기 때문에 get(0)를 통해서 반환합니다.
+         * 해당하는 값이 없을 경우 null을 반환하고,
+         * 이는 컨트롤러의 {@link com.ktspace.miniHomeIoT.controller.HomeIoTController#getResultCode(ResponseDTO) getResultCode}로
+         * 예외처리 됩니다.
+         */
+        DeviceStatusDTO result = deviceStatusList.get(0);
         return result;
     }
 
+    /**
+     * 호출은 {@link com.ktspace.miniHomeIoT.controller.HomeIoTController#getUserDevices(String) getUserDevices} 참조
+     * DB조회는 {@link DeviceMapper#findDvcList(DeviceVO) findDvcList} 참조
+     * userId 에 해당하는 모든 장치 정보를 조회합니다.
+     * @param userId
+     * @return 해당 사용자의 모든 장치에 대한 정보를 반환
+     */
     public HashMap<String, Object> getUserDevices(String userId) {
         HashMap<String, Object> result = new HashMap<>();
 
@@ -44,22 +85,47 @@ public class HomeIoTService {
         return result;
     }
 
+    /**
+     * 요청된 장치 sequence 정보, 수정할 리소스 정보를 토대로 DB 정보를 수정합니다.
+     * @param userId
+     * @param dvcSeq
+     * @param rscGroup
+     * @param value
+     * @return 성공 여부 반환
+     */
     public HashMap<String, Object> controlResource(String userId, Integer dvcSeq, String rscGroup, String value) {
-        Integer updatedRowCnt = resourceMapper.updateRscValueByDvcSeq(userId, dvcSeq, rscGroup, value);
         HashMap<String, Object> result = new HashMap<String, Object>();
+        /**
+         * myBatis를 통해 쿼리문을 실행합니다.
+         * 또한 updeate된 row의 개수를 반환하기 때문에
+         * 이를 통해 성공 여부를 판단합니다.
+         */
+        Integer updatedRowCnt = resourceMapper.updateRscValueByDvcSeq(userId, dvcSeq, rscGroup, value);
 
         if (updatedRowCnt == 0){
             result.put("resultCode", FAIL_CODE);
             result.put("resultMessage", FAIL_MSG);
             return result;
+        } else{
+            result.put("resultCode", SUCCEED_CODE);
+            result.put("resultMessage", SUCCEED_MSG);
+            /**
+             * 리소스 정보 수정 성공 시, 해당하는 리소스 로그도 남깁니다.
+             */
+            resourceMapper.insertRscLog(dvcSeq, rscGroup, value);
         }
 
-        result.put("resultCode", SUCCEED_CODE);
-        result.put("resultMessage", SUCCEED_MSG);
-        resourceMapper.insertRscLog(dvcSeq, rscGroup, value);
         return result;
     }
 
+    /**
+     * 요청된 장치 sequence, 사용자 정보를 통해
+     * 해당 사용자가 소유한 장치라면
+     * 해당 장치를 삭제합니다.
+     * @param userId
+     * @param dvcSeq
+     * @return 성공 여부를 반환합니다.
+     */
     public Map<String, Object> deleteDevice(String userId, Integer dvcSeq) {
         Map<String, Object> result = new HashMap<>();
         Integer deletedRowCnt = deviceMapper.deleteDvcByDvcSeq(userId, dvcSeq);
