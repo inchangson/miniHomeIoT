93221d0340279d70c8f6943c91b34ca9434c8c49
inchang <3721cha21.11@gmail.com>
2022-08-08 07:51:49 +0900

    Fix : remove Generic in DeviceListResponse

--- src/main/java/com/ktspace/miniHomeIoT/dto/response/DeviceListResponse.java
+++ src/main/java/com/ktspace/miniHomeIoT/dto/response/DeviceListResponse.java
@@ -1,16 +1,17 @@
 package com.ktspace.miniHomeIoT.dto.response;
 
+import com.ktspace.miniHomeIoT.dto.Device;
 import com.ktspace.miniHomeIoT.dto.response.GeneralResponse;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
-public class DeviceListResponse<T> extends GeneralResponse {
+public class DeviceListResponse extends GeneralResponse {
     ListData data;
 
     class ListData {
         int deviceCount;
-        List<T> deviceList;
+        List<Device> deviceList;
     }
 }
